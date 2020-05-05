18db71d456abbda48e7a07c34e2d21c0c4079941
ibrahima <iboutall98@gmail.com>
2020-05-05 22:27:29 +0000

    simulation dhcp

---
+++ dhcp.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include "donneesCommunes.c"
+#include "ajouter.c"
+
+listeMachine *DHCP (listeMachine *reseau, Machine *m1){
+    srand(time(NULL));
+    int number, unique = 0;
+    char* ip = "";
+    char* lastDigits = "";
+    printf("Tentative de connection...\n");
+    listeMachine *positionMachine  (listeMachine *listemachine);
+    listeMachine *positionFinale = positionMachine (reseau);
+    strcpy(ip, positionFinale->machine->adresseIp + 10); 
+    do{
+        number = rand()%255;
+        sprintf(lastDigits, "%d", number); //conversion de int a char
+        strcat(ip , lastDigits);
+        for (listeMachine *tmp = reseau; tmp != NULL; tmp = tmp->next){
+            if (tmp->machine->adresseIp = ip){
+                unique = unique++;
+            }
+        }
+        if (unique = 0){
+            positionFinale->next = m1;
+        }
+    }while(unique != 0);
+    printf("Connexion réussit.\n");
+    return reseau;
+}
