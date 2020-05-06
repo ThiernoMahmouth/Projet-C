4a2517990438a6ec4414db95fa72e356f4bd5da2
ibrahima <iboutall98@gmail.com>
2020-05-06 21:38:55 +0000

    modification dhcp

--- dhcp.c
+++ dhcp.c
@@ -6,19 +6,20 @@
 #include "donneesCommunes.c"
 #include "ajouter.c"
 
-listeMachine *DHCP (listeMachine *reseau, Machine *m1){
+Reseau *DHCP (Reseau *reseau, Machine *m1){
     srand(time(NULL));
-    int number, unique = 0;
+    int number = 0, unique = 0;
     char* ip = "";
     char* lastDigits = "";
     printf("Tentative de connection...\n");
     listeMachine *positionMachine  (listeMachine *listemachine);
-    listeMachine *positionFinale = positionMachine (reseau);
-    strcpy(ip, positionFinale->machine->adresseIp + 10); 
+    listeMachine *positionFinale = positionMachine (reseau->liste_machines);
+    strcpy (ip, reseau->addrese);
+    ip[strlen(ip)-1] = 0; //suprime le dernier charactere de l'adresse donc le 0
     do{
         number = rand()%255;
         sprintf(lastDigits, "%d", number); //conversion de int a char
-        strcat(ip , lastDigits);
+        strcat(ip ,lastDigits);
         for (listeMachine *tmp = reseau; tmp != NULL; tmp = tmp->next){
             if (tmp->machine->adresseIp = ip){
                 unique = unique++;
