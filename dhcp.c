41fbb0c4bb380c6352850b27077cf0de6e5abb19
ibrahimaTALL1998 <51260695+ibrahimaTALL1998@users.noreply.github.com>
2020-05-09 16:47:57 +0000

    Update dhcp.c

--- dhcp.c
+++ dhcp.c
@@ -3,30 +3,32 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
-//#include "donneesCommunes.c"
+#include "donneesCommunes.c"
 #include "ajouter.c"
 
-listeMachine *DHCP (listeMachine *reseau, Machine *m1){
+Reseau *DHCP (Reseau *reseau, Machine *m1){
     srand(time(NULL));
-    int number, unique = 0;
+    int number = 0, unique = 0;
     char* ip = "";
     char* lastDigits = "";
     printf("Tentative de connection...\n");
     listeMachine *positionMachine  (listeMachine *listemachine);
-    listeMachine *positionFinale = positionMachine(reseau);
-    strcpy(ip, positionFinale->machine.adresseIp + 10); 
+
+    listeMachine *positionFinale = positionMachine (reseau->liste_machines);
+    strcpy (ip, reseau->addrese);
+    ip[strlen(ip)-1] = 0; //suprime le dernier charactere de l'adresse donc le 0
     do{
         number = rand()%255;
         sprintf(lastDigits, "%d", number); //conversion de int a char
-        strcat(ip , lastDigits);
+        strcat(ip ,lastDigits);
         listeMachine *m=malloc(sizeof(listeMachine));
         if(m!= NULL)
         {
             m->machine=m1;
             m->next=NULL;
         }
-        for (listeMachine *tmp = reseau; tmp != NULL; tmp = tmp->next){
-            if (tmp->machine.adresseIp = ip){
+        for (listeMachine *tmp = reseau->liste_machines; tmp != NULL; tmp = tmp->next){
+            if (!strcmp(tmp->machine.adresseIp, ip)){
                 unique = unique++;
             }
         }
